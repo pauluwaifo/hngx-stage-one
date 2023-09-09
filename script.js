ab6fbe2c0fd3055e1014db1b77421646f87d43d3
pauluwaifo <127699612+pauluwaifo@users.noreply.github.com>
2023-09-09 07:09:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function updateTime() {
     const hours = String(currentDate.getUTCHours()).padStart(2, '0');
     const minutes = String(currentDate.getUTCMinutes()).padStart(2, '0');
     const seconds = String(currentDate.getUTCSeconds()).padStart(2, '0');
-    const timeString = `Current UTC Time: ${hours}:${minutes}:${seconds} UTC`;
+    const milliseconds = String(currentDate.getUTCMilliseconds()).padStart(3, '0');
+    const timeString = `Current UTC Time: ${hours}:${minutes}:${seconds}.${milliseconds} UTC`;
     const timeElement = document.getElementById('time');
     timeElement.textContent = timeString;
-
 }
 
 function Day() {
     const currentDate = new Date();
-    const currentDayNumber = currentDate.getUTCDay(); // Use getUTCDay() to get UTC day
+    const currentDayNumber = currentDate.getUTCDay(); 
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const currentDay = daysOfWeek[currentDayNumber]
     const dayElement = document.getElementById('day');
@@ -20,4 +20,4 @@ function Day() {
 
 updateTime()
 Day()
-setInterval(updateTime, 1000)
+setInterval(updateTime, 1)
